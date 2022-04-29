48f46fb36d58b631881085c1883962defc43211b
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-29 12:36:05 +0100

    Add loop to memory to stress it

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,9 @@
 pub fn run() {
     println!("Memory running");
+
+    let mut growable_vec: Vec<i128> = Vec::new();
+
+    for number in 0..1_000_000_000 {
+        growable_vec.push(number);
+    }
 }

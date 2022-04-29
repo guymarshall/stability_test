12a3aa4ad8ef59adac79d6c6a036b6f6e4617abb
Guy Marshall <guymarshall9@gmail.com>
2022-04-29 21:13:30 +0100

    Remove network from list

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod cpu;
 mod memory;
 mod storage;
-mod network;
 
 fn main() {
     println!("This stability test is split up into multiple different parts: CPU (single-thread), RAM, Storage & Network.\n");
     cpu::run();
-    // memory::run();
-    network::run();
+    memory::run();
     storage::run();
 }

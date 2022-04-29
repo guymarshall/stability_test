2261a4842cf7b5e883145d1ffbd459f617bf54ec
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-29 12:36:24 +0100

    Remove unnecessary comments

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,8 @@ mod network;
 
 fn main() {
     println!("This stability test is split up into multiple different parts: CPU (single-thread), RAM, Storage & Network.\n");
-    // CPU
     cpu::run();
-    // RAM
     memory::run();
-    // Storage
     storage::run();
-    // Network
     network::run();
 }

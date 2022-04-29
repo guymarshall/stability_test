ce4866ef3fba627341abefdfcc1617ae59479536
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-29 12:45:11 +0100

    Implement brute force prime number check (freezes)

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,24 @@
 pub fn run() {
     println!("CPU running");
+
+    println!("Prime numbers:");
+    let mut prime_numbers: Vec<u128> = Vec::new();
+
+    for i in 2..1_000_000 {
+        if is_prime(i) == 1 {
+            prime_numbers.push(i);
+        }
+    }
+
+    println!("Prime numbers: {:?}", prime_numbers);
+}
+
+fn is_prime(number: u128) -> u8 {
+    for i in 2..number {
+        if number % i == 0 {
+            return 0;
+        }
+    }
+
+    return 1;
 }

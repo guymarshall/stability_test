15a9a7ef5a5527b400f91c08bd5f8996b950095a
Guy Marshall <guymarshall9@gmail.com>
2022-04-30 11:11:42 +0100

    Remove storage from main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod cpu;
 mod memory;
-mod storage;
 
 fn main() {
     println!("This stability test is split up into multiple different parts: CPU (single-thread), RAM, Storage & Network.\n");
     cpu::run();
     memory::run();
-    storage::run();
 }
